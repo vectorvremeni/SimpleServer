df6f4fecf4556f8f17acdc4f25b8638f2d4dc832
vectorvremeni <vremenivector>
2021-03-21 11:00:50 +0300

    project dependencies changed and resolved

---
+++ ConsoleGame/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TheGame;
+
+namespace ConsoleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleRenderer r = new ConsoleRenderer();
+
+            Game g = new Game(r);
+            g.Init(5, 5);
+
+            g.MoveUser("RIGHT");
+            g.MoveUser("RIGHT");
+
+            String temp = g.RenderField();
+
+            Console.WriteLine(temp);
+        }
+    }
+}
---
+++ GNGame/The_GNGame.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace GNGame
+{
+    public class The_GNGame
+    {
+        public static string Gr_lose = "lose";
+        public static string Gr_less = "less";
+        public static string Gr_more = "more";
+        public static string Gr_equal = "equal";
+        
+        Random rnd = new Random(DateTime.Now.Millisecond);
+
+        int TheNumber = 0;
+        int TryCounter = 0;
+        int MaxTry = 0;
+        int GetRandom()
+        {
+            return rnd.Next(1,80);
+        }
+
+        public void init(int trycount)
+        {
+            TheNumber = GetRandom();
+            MaxTry = trycount;
+            TryCounter = 0;
+        }
+
+        private string tryguess(int user_input)
+        {
+            if(this.TheNumber == user_input)
+            {
+                return Gr_equal;
+            }
+            else if(this.TheNumber < user_input)
+            {
+                return Gr_more;
+            }
+            else
+            {
+                return Gr_less;
+            }
+        }
+        public string guess(int user_input)
+        {
+
+            if (TryCounter < MaxTry-1)
+            {
+                this.TryCounter++;
+                return tryguess(user_input);
+            }
+            else
+            {
+                return Gr_lose;
+            }
+
+        }
+        public void init_test(int thenumber)
+        {
+            this.TheNumber = thenumber;
+        }
+    }
+}
