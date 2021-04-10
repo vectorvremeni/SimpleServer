6b7154128e0697cd90347fe317dfb04a35a1e9d5
vectorvremeni <vremenivector>
2021-04-10 17:15:39 +0300

    testcontroller

---
+++ MVC/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.MVC
+{
+    public class Test:Controller
+    {
+        public String Index()
+        {
+            return "Hi frfom Test:Index";
+        }
+
+        public String Test2()
+        {
+            return "Hi frfom Test2:TEST!!!!!!!!!!!";
+        }
+    }
+}
